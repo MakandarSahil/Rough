3c0fb347ccc809640e0469f58dd5ec30984a6bba
MakandarSahil <sahilmakandar15@gmail.com>
2025-07-18 10:32:09 +0530

    debounce hook

--- Storage/server/dist/middlewares/error.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({
-        message: 'Something went wrong. Please try again later.',
-    });
-};
